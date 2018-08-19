19b753421afa71d33ccfea8b8f917e130624c95b
fernandat <fefedel9@hotmail.com>
2018-08-19 16:16:20 -0300

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, OnDestroy, AfterViewInit, AfterContentChecked, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    console.log('OnInit');
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavBarComponent } from './shared/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
